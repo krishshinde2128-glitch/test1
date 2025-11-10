b51e45cbfe51f293daf06410bb7ef235eed70ef0
krishshinde2128-glitch <krishshinde2128@gmail.com>
2025-11-10 22:18:43 +0530

    birthday

---
+++ problem/birthday.py
@@ -0,0 +1,22 @@
+import random
+
+people = 10
+
+trials = 10000
+
+same_birthday_count = 0
+
+for trial in range(trials):
+    birthdays = []
+
+    for _ in range(people):
+        day = random.randint(1, 365)
+        birthdays.append(day)
+    
+    if len(birthdays) != len(set(birthdays)):
+        same_birthday_count += 1
+
+probability = same_birthday_count / trials
+
+print(f"Out of {trials} simulations, {same_birthday_count} had at least two people sharing a birthday.")
+print(f"Estimated Probability: {probability:.4f}")
